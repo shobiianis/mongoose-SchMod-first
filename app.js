9df2c3ed4b2985240577262c159b18b65c94d257
shoaib anis <shoaibwork2000@gmail.com>
2023-01-10 22:44:32 +0500

    sc

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
- const mongoose=require("mongoose");
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/e-com")
 
-const main= async()=>{
+const productSchema=new mongoose.Schema({
+   name:String,
+   ram:String,
+   price:Number,
 
-    await mongoose.connect("mongodb://localhost:27017/e-com")
-    const productSchema=new mongoose.Schema({
-       name:String
+})
 
-    })
+const saveInDb= async()=>{
+
 
     const productModel=mongoose.model("product",productSchema);
-    let data=new productModel({name :"huwaie"}); //it will enter name in mongodb Database    
+    let data=new productModel({name :"abc",ram:"16gb",price:40000});
+
     let result=await data.save();
     console.log(result)
 }
 
-// let data=new productModel({name:"ipone",price:9000}) this will  enter name only as in schema we have defined only one field for products collection in ecom database
-// what if we want to enter two fields so for this we just another field name and their datatype example:
-/*
-const productSchema=new mongoose.Schema({
-    name:String,
-    price:Number
-
-})
-*/
-
-main();
 
 
+const updateInDb=async()=>{
+    const productModel=mongoose.model("product",productSchema);//same as for saveInDb   
+    let data= await productModel.updateOne(
+           {name:"huwaie"}, // it will update where name is huwaie 
+           {$set : {price:9000}}// and update price
+
+    )
+    console.log(data)
+}
+
+const deleteInDb=async()=>{
+
+    const productModel=mongoose.model("product",productSchema);//same as for saveInDb   
+    let data= await productModel.deleteOne({name:"iphone"})// it will delete whole entry for where name is iphone
+     console.log(data);
+    
+}
+
+const findInDb= async ()=>{
+
+    const productModel=mongoose.model("product",productSchema);//same as for saveInDb   
+    let data= await productModel.find(); //it will read the whole database collections
+    console.log(data)
+}
+const findSpecific=async()=>{
+
+    const productModel=mongoose.model("product",productSchema);//same as for saveInDb   
+    let data= await productModel.find({name: "iphone"}); //it will read only for that record where name is iphone
+    console.log(data)
+}
 
 
 
